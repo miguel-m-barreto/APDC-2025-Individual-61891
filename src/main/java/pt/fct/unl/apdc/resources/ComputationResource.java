427fdc72878e45c7b5c7f7dad4553f525be61711
mikebar12 <miguel.m.barreto.dev@gmail.com>
2025-04-11 03:02:41 +0100

    Update ComputationResource.java
    TriggerExecuteComputeTask was not with the correct logic

--- src/main/java/pt/fct/unl/apdc/resources/ComputationResource.java
+++ src/main/java/pt/fct/unl/apdc/resources/ComputationResource.java
@@ -11,6 +11,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
@@ -55,8 +56,31 @@ public class ComputationResource {
 		LOG.fine("Replying to date request.");
 		return Response.ok().entity(g.toJson(fmt.format(new Date()))).build();
 	}
-	
+
 	@GET
+	@Path("/compute")
+	public Response triggerExecuteComputeTask() throws IOException {
+		String projectId = "shining-expanse-453014-c4";
+		String queueName = "Default";
+		String location = "europe-west6";
+		LOG.log(Level.INFO, projectId + " :: " + queueName + " :: " + location );
+
+		try (CloudTasksClient client = CloudTasksClient.create()) {
+			String queuePath = QueueName.of(projectId, location, queueName).toString();
+			Task.Builder taskBuilder = 
+				Task.newBuilder().setAppEngineHttpRequest(AppEngineHttpRequest.newBuilder()
+						.setRelativeUri("/rest/utils/compute").setHttpMethod(HttpMethod.POST)
+						.build());
+
+			taskBuilder.setScheduleTime(Timestamp.newBuilder().setSeconds(Instant.now(Clock.systemUTC()).getEpochSecond()));
+			
+			client.createTask(queuePath, taskBuilder.build());
+		} 
+		
+		return Response.ok().build();
+	}
+
+	@POST
 	@Path("/compute")
 	public Response executeComputeTask() {
 		LOG.fine("Starting execute computation task.");
@@ -69,26 +93,4 @@ public class ComputationResource {
 		}
 		return Response.ok().entity("Computation task completed").build();
 	}
-
-	@GET
-	@Path("/triggerCompute")
-	public Response triggerExecuteComputeTask() throws IOException {
-		String projectId = "quantum-shard-415522";
-		String queueName = "Default";
-		String location = "europe-west6";
-		LOG.log(Level.INFO, projectId + " :: " + queueName + " :: " + location );
-
-		try (CloudTasksClient client = CloudTasksClient.create()) {
-			String queuePath = QueueName.of(projectId, location, queueName).toString();
-			Task.Builder taskBuilder = Task.newBuilder().setAppEngineHttpRequest(AppEngineHttpRequest.newBuilder()
-							.setRelativeUri("/rest/utils/compute").setHttpMethod(HttpMethod.POST).build());
-
-			taskBuilder.setScheduleTime(Timestamp.newBuilder().setSeconds(Instant.now(Clock.systemUTC()).getEpochSecond()));
-			
-			client.createTask(queuePath, taskBuilder.build());
-		} 
-		return Response.ok().build();
-	}
-
-	
 }
